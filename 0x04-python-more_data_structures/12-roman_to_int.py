c284d6e7fd6d527c085b14450609954ce42c1024
nzejoe <jonathannzete@gmail.com>
2023-11-08 07:22:08 -0800

    function that convert roman numeral

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str):
+        return 0
+    roman_numerals = {
+                    'I': 1,
+                    'V': 5,
+                    'X': 10,
+                    'L': 50,
+                    'C': 100,
+                    'D': 500,
+                    'M': 1000
+                    }
+    total = 0
+    prev_value = 0
+
+    for char in reversed(roman_string):
+        prev_value = roman_numerals[char]
+        total += prev_value if total < prev_value * 5 else -prev_value
+    return total
