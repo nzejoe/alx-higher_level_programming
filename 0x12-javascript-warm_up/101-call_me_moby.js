1ac032480f68442274458ad2d4400b6fc63e8dfc
nzejoe <jonathannzete@gmail.com>
2024-02-12 08:24:27 -0800

    15. Call me Moby fix

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
 
-exports.addMeMaybe = function (number, theFunction) {
-  theFunction(number + 1);
+exports.callMeMoby = function (x, theFunction) {
+  for (let y = 0; y < x; y++) {
+    theFunction();
+  }
 };
